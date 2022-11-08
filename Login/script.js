c7f04553ed5b7ddb541bf9f596f123c80f4b55f4
danielaustrierrrrr <daniel.c14@gso.schule.koeln>
2022-11-08 13:34:39 +0100

    meldungen hinzugefügt

--- Login/script.js
+++ Login/script.js
@@ -1,1 +1,17 @@
-document.getElementsByTagName("h1")[0].style.fontSize = "6vw";
+function login(){
+    let uname = document.getElementById('username');
+    let pwd = document.getElementById('password');
+
+    if (!pwd.value && !uname.value)
+        {
+            alert('Geben Sie Ihre Anmeldedaten an')
+        }    
+    else if(!uname.value)
+		{
+			alert("Geben Sie Ihren Benutzernamen ein");
+		}
+	else if (!pwd.value)
+		{
+        	alert("Geben Sie Ihr Passwort ein");
+		}
+}
